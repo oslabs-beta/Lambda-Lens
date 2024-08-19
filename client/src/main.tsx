c9d79163d644471b7e92f2c22de3d5e636d69707
Dylan Kinsella <dylankinsella@Dylans-MacBook-Pro.local>
2024-08-19 17:54:38 -0600

    created a few simple example charts and a page to display them

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import App from './App.tsx';
+import ChartsPage from './ChartsPage.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/charts' element={<ChartsPage />} />
+      </Routes>
+    </Router>
   </StrictMode>,
-)
+);
