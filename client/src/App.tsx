168485748aac86b27022e434b1aebab8c1e36b68
Erin Lee <erinlee@MacBook-Air-3.local>
2024-08-24 09:45:06 -0700

    initial navbar styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DashboardContainer from './containers/DashboardContainer';
 import CloudwatchContainer from './containers/CloudwatchContainer';
 import NavbarComponent from './components/NavbarComponent';
-import './App.css'
+import './App.scss'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -13,7 +13,7 @@ function App() {
   return (
     <Router>
       <div>
-        <NavbarComponent />
+        <NavbarComponent id={'navbar'}/>
         <Routes>
           <Route path='/' element={<DashboardContainer />} />
           <Route path='/cloudwatchmetrics' element={<CloudwatchContainer />} />
