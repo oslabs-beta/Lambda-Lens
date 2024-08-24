0c9e4ce9059af2caf94492b47d031557861a6ec4
Erin Lee <erinlee@MacBook-Air-3.local>
2024-08-24 12:02:48 -0700

    resolved merge conflict

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-<<<<<<< HEAD
+
 import App from './App.js';
 import './App.scss';
-=======
-import App from './App';
-import './index.css';
->>>>>>> dev
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
