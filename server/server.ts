0fe771819ce0d9e2e2e7f387d1787778869f5bdc
Spencer <spencer@spency.local>
2024-08-26 14:10:54 -0700

    getFunctionsController in typescript

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { getLogs } from './controllers/lambdaController';
+import { getFunctionsController } from './controllers/getFunctionsController';
+import { get } from 'http';
 
 const app = express();
 
@@ -11,6 +13,13 @@ getLogs().then(() => {
   console.log('Log fetching completed');
 }).catch(err => {
   console.log('Error fetching logs:', err.message)
+})
+
+
+//to test, run npm start in the server direcotry then send a postman request to localhost:8080/api
+app.get('/api', getFunctionsController.listFunctionData, (_req: Request, res: Response, _next: NextFunction) => {
+  console.log('res.locals.functionsList from server.ts: ', res.locals.functionsList);
+  res.send(res.locals.functionsList);
 })
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
