15a72982b5a51530d55a424aacd3b982c2dc522e
FT49 <ft49@FT49s-iMac.local>
2024-08-27 15:15:17 -0700

    refine logo

--- client/src/components/NavbarComponent.tsx
+++ client/src/components/NavbarComponent.tsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import '../NavbarComponent.css'
+import logo from '../logo.png'
 
 // const NavbarComponent = () => {
 //   return (
@@ -19,6 +21,9 @@ import { Link } from 'react-router-dom';
 const NavbarComponent = () => {
   return (
     <nav className='navbar'>
+      <div className='logo-container'>
+        <img src={logo} alt='Logo' className='logo' />
+      </div>
       <ul id='navbarLinks'>
         <li>
           <Link to='/'>Dashboard</Link>
@@ -35,3 +40,62 @@ const NavbarComponent = () => {
 };
 
 export default NavbarComponent;
+// import React, { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
+// import '../NavbarComponent.css';
+// import logo from '../logo.png';
+
+// const NavbarComponent = () => {
+//   const [darkMode, setDarkMode] = useState(false);
+
+//   useEffect(() => {
+//     const savedTheme = localStorage.getItem('theme');
+//     if (savedTheme === 'dark') {
+//       setDarkMode(true);
+//       document.body.classList.add('darkmode');
+//     }
+//   }, []);
+
+//   const toggleDarkMode = () => {
+//     setDarkMode(!darkMode);
+//     if (!darkMode) {
+//       document.body.classList.add('darkmode');
+//       localStorage.setItem('theme', 'dark');
+//     } else {
+//       document.body.classList.remove('darkmode');
+//       localStorage.removeItem('theme');
+//     }
+//   };
+
+//   return (
+//     <nav className='navbar'>
+//       <div className='logo-container'>
+//         <img src={logo} alt='Logo' className='logo' />
+//       </div>
+//       <ul id='navbarLinks'>
+//         <li>
+//           <Link to='/'>Dashboard</Link>
+//         </li>
+//         <li>
+//           <Link to='/cloudwatchmetrics'>Cloudwatch Metrics</Link>
+//         </li>
+//         <li>
+//           <Link to='/configuration'>Configuration</Link>
+//         </li>
+//       </ul>
+//       <div id='theme-switch' onClick={toggleDarkMode}>
+//         {darkMode ? (
+//           <svg /* Dark mode icon SVG */>
+//             {/* SVG path data for dark mode */}
+//           </svg>
+//         ) : (
+//           <svg /* Light mode icon SVG */>
+//             {/* SVG path data for light mode */}
+//           </svg>
+//         )}
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default NavbarComponent;
