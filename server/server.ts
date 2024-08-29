a64b3554ecda779aa462f0dddf1cdcc42ca0bc51
FT49 <ft49@FT49s-iMac.local>
2024-08-29 08:45:30 -0700

    update

--- server/server.ts
+++ server/server.ts
@@ -3,33 +3,47 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectToDatabase from './models/dbConnection';
-import lambdaController from './controllers/lambdaController'; 
+import lambdaController from './controllers/lambdaController';
 import { getFunction } from './controllers/getFunctionsController';
 import configRoutes from './routes/configRoutes';
+import dataRoutes from './routes/dataRoutes';
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.json()); 
-app.use(cors()); 
-app.use(express.json()); 
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/config', configRoutes);
+connectToDatabase()
+  .then(() => {
+    console.log('Database connected');
+  })
+  .catch((err) => {
+    console.error('Database connection error:', err);
+    process.exit(1);
+  });
 
-connectToDatabase().then(() => {
-  console.log('Database connected');
-}).catch(err => {
-  console.error('Database connection error:', err);
-  process.exit(1);
-});
+// app.get('/api', getFunction, (_req: Request, res: Response, _next: NextFunction) => {
+//   console.log('res.locals.functionsList from server.ts: ', res.locals.functionsList);
+//   res.status(200).send(res.locals.functionsList);
+// });
 
+// app.get('/logs', lambdaController.processLogs, (req: Request, res: Response) => {
+//   const alldata = res.locals.alldata;
+//   res.json(alldata);
+// });
+app.use(
+  '/api/config',
+  configRoutes,
+  (_req: Request, res: Response, _next: NextFunction) => {
+    return res.status(200).json(res.locals.saved);
+  }
+);
 
-app.get('/logs', lambdaController.processLogs, (req: Request, res: Response) => {
-  const alldata = res.locals.alldata;
-  res.json(alldata);
-});
+app.use('/data', dataRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello');
@@ -38,7 +52,6 @@ app.get('/', (req: Request, res: Response) => {
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ error: err.message });
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
