88fa84abd70b0169d54d44f6365ce4d575378e03
Dylan Kinsella <dylankinsella@Dylans-MacBook-Pro.local>
2024-09-11 14:30:58 -0600

    deletes unused assets and updates readme

--- client/src/components/NavbarComponent.tsx
+++ client/src/components/NavbarComponent.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../NavbarComponent.css';
-import lambda from '../lambda.png';
+import lambda from '../assets/lambda.png';
 
 const LightModeIcon = () => (
   <svg
