4bd95ec888498edcc2c612e95aadc22fcf5e3cf0
dkinsell <dylan.kinsella7@gmail.com>
2025-03-12 21:26:13 -0600

    refactor: update import path for databaseController to match naming convention

--- server/routes/dataRoutes.ts
+++ server/routes/dataRoutes.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import { databaseController } from '../controllers/databaseController';
+import { databaseController } from '../controllers/DatabaseController';
 import metricsController from '../controllers/MetricsController';
 import chatController from '../controllers/ChatController';
 
