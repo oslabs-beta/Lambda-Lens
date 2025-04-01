4d0ac5bd8cefc846059026333d6f35acd591f3a5
dkinsell <dylan.kinsella7@gmail.com>
2025-03-31 20:32:16 -0600

    feat: add Firebase token verification to chat and config routes

--- server/routes/chatRoutes.ts
+++ server/routes/chatRoutes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 import chatController from '../controllers/ChatController';
+import { verifyFirebaseToken } from '../middleware/authMiddleware'; 
 
 const chatRouter = Router();
+
+chatRouter.use(verifyFirebaseToken);
+
 chatRouter.post('/', chatController.handleChat);
 
 export default chatRouter;
