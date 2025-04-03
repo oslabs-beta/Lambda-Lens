31c2a45ff22d01c50ded83897e7f717e1b505310
dkinsell <dylan.kinsella7@gmail.com>
2025-04-03 13:16:41 -0600

    fix: update firebase configuration to use environment variables

--- client/src/firebaseConfig.ts
+++ client/src/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBGgrKdxoI8vUcdnbWD1lD4pDkhEawLxkc",
-    authDomain: "lambda-lens.firebaseapp.com",
-    projectId: "lambda-lens",
-    storageBucket: "lambda-lens.firebasestorage.app",
-    messagingSenderId: "395532899337",
-    appId: "1:395532899337:web:5a1f794afeff2d8a9654d8",
-    measurementId: "G-54B4Q56ZZ2"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
